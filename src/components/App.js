b8d2ef59ebd1e4373aedaf0b13d3518220dbf526
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-02-20 15:53:48 -0500

    Rendered containers, tiles, set up props and stage

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import ButtonContainer from './ButtonContainer'
+import PigContainer from './PigContainer'
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      pigs: []
+    }
+  }
+
+  fetchHogs = () => {
+    this.setState({
+      pigs: hogs
+    })
+  }
+
   render() {
     return (
       <div className="App">
-          < Nav />
-
+          <Nav />
+          <ButtonContainer fetchHogs={this.fetchHogs}/>
+          <PigContainer pigs = {this.state.pigs}/>
       </div>
     )
   }
